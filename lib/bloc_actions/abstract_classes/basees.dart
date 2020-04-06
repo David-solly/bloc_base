f6bec9d02f3bf4e7bc1911a9c8cf15851836fb9c
David-solly <David.solomon_@outlook.com>
2020-04-06 02:10:01 +0100

    cleaned abstracts

--- lib/bloc_actions/abstract_classes/basees.dart
+++ lib/bloc_actions/abstract_classes/basees.dart
@@ -3,18 +3,14 @@ import 'package:bloc_base/bloc_pipe.dart';
 /// Provides a base for [Bloc] class functions
 /// to implement `actions` through
 abstract class BlocActions {
-  final dataEvent;
-  final StreamEventHandler handler;
-
-  BlocActions(this.dataEvent, {this.handler});
+  const BlocActions();
 }
 
 class ProcessAction extends BlocActions {
   final dataEvent;
   final StreamEventHandler handler;
 
-  ProcessAction(this.dataEvent, {this.handler})
-      : super(dataEvent, handler: handler);
+  ProcessAction(this.dataEvent, {this.handler});
 }
 
 /// Provides a base for [Bloc] class functions
