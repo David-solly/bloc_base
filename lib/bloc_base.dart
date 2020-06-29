27cfba2146c769175b088877821e3b58dfffa375
David-solly <David.solomon_@outlook.com>
2020-06-29 23:06:08 +0100

    added legacy support for direct sink access

--- lib/bloc_base.dart
+++ lib/bloc_base.dart
@@ -1,9 +1,20 @@
 library bloc_base;
 
+import 'dart:async';
 
 /// An abstract class to provide an interface for bloc classes to implement
 abstract class BlocBase<K, T> {
-  void dispose();
+  void dispose() {
+    if (dataRequestSink != null) dataRequestSink.close();
+  }
+
+  /// A [Stream] exposed by all blocs of extending this type
+  /// This allows functions to interact with blocs using the same APIs
+  Stream dataStream;
+
+  /// A [StreamSink] exposed by all blocs of extending this type
+  /// This allows functions to interact with blocs using the same APIs
+  StreamSink dataRequestSink;
 }
 
 abstract class BlocPipeSpec {
